c4f33b6272772492873d24a56251843a52eeebb6
Bill Driscoll <bill.driscoll22@gmail.com>
2016-05-25 16:58:14 -0700

    turning to iterative

--- router.nim
+++ router.nim
@@ -12,48 +12,76 @@ type
 
   Handler* = proc(request: nhttp.Request, response: nhttp.Response)
 
-proc getHandler(this: Router, currentNode: PathNode,
-                routeComponents: seq[string]): Handler =
-  if len(routeComponents) == 0:
-    return currentNode.handler
-  var currentComponent = routeComponents[0]
-  let child = currentNode.children.getOrDefault(currentComponent)
-  if(child != nil):
-    let slicedComponents = routeComponents[1..routeComponents.high()]
-    let child = currentNode.children[currentComponent]
-    return this.getHandler(child, slicedComponents)
-  else:
-    return this.notFound
+proc getHandler(this: Router, routeComponents: seq[string]): Handler =
+  var currentNode = this.root
+  for i, routeComponent in routeComponents:
+    if(i == routeComponents.len() - 1):
+      return currentNode.handler
+    else:
+      currentNode = currentNode.children.getOrDefault(routeComponent)
+      if(currentNode == nil):
+        return this.notFound
 
 proc initNode(value: string, handler: Handler): PathNode =
   var children = tables.initTable[string, PathNode]()
   result = PathNode(value: value, children: children, handler: handler )
 
 proc routeComponents(this: Router, methd: string, path: string): seq[string] =
-  result = (methd.toLower() & path).split("/")
+  result = (methd.toUpper() & path).split("/")
 
-proc addRoute(this: Router, currentNode: var PathNode,
-              routeComponents: seq[string], handler: Handler) =
-  if len(routeComponents) == 0:
-    return
-  let currentComponent = routeComponents[0]
-  var child = currentNode.children.getOrDefault(currentComponent)
-  if(child != nil):
-    let slicedComponents = routeComponents[1..routeComponents.high()]
-    this.addRoute(child, slicedComponents, handler)
-  else:
-    var newNode = initNode(currentComponent, handler)
-    currentNode.children[currentComponent] = newNode
-    this.addRoute(newNode, routeComponents[1..routeComponents.high()], handler)
+proc addRoute(this: Router, routeComponents: seq[string], handler: Handler) =
+  # if len(routeComponents) == 0:
+  #   return
+  # let currentComponent = routeComponents[0]
+  # var child = currentNode.children.getOrDefault(currentComponent)
+  # if(child != nil):
+  #   let slicedComponents = routeComponents[1..routeComponents.high()]
+  #   this.addRoute(child, slicedComponents, handler)
+  # else:
+  #   var newNode = initNode(currentComponent, handler)
+  #   currentNode.children[currentComponent] = newNode
+  #   this.addRoute(newNode, routeComponents[1..routeComponents.high()], handler)
+  var currentNode = this.root
+  for i, routeComponent in routeComponents:
+    echo "i: "
+    echo i
+    echo "route component: "
+    echo routeComponent
+    echo "current node: "
+    echo currentNode.value
+    if(i == routeComponents.len() - 1):
+      echo "first if statement"
+      echo "value of current node getting handler attached"
+      echo currentNode.value
+      currentNode.handler = handler
+      return
+    else:
+      echo "checking current node's children"
+      var childNode = currentNode.children.getOrDefault(routeComponent)
+      if(childNode == nil):
+        echo "2nd if statement (i.e. the new value was not yet in the tree)"
+        echo "value of new node: "
+        var newNode = initNode(routeComponent, nil)
+        echo newNode.value
+        echo "adding new node to current node's children"
+        currentNode.children[routeComponent] = newNode
+        echo "swapping current node for new node"
+        echo "value of current node"
+        currentNode = newNode
+        echo currentNode.value
+      else:
+        currentNode = childNode
+
+
 
 proc handle*(this: Router, request: nhttp.Request, response: nhttp.Response) =
   let path = request.uri.path
   let methd = request.m
-  let handler = this.getHandler(this.root, this.routeComponents(methd, path))
+  let handler = this.getHandler(this.routeComponents(methd, path))
   handler(request, response)
 
 proc add*(this: var Router, methd: string, path: string, handler: Handler) =
-  this.addRoute(this.root, this.routeComponents(methd, path), handler)
+  this.addRoute(this.routeComponents(methd, path), handler)
 
 proc initRouter*(notFound: Handler): Router =
   var children = tables.initTable[string, PathNode]()
--- router_test.nim
+++ router_test.nim
@@ -9,11 +9,20 @@ proc rootGetHandler(request: nhttp.Request, response: nhttp.Response) =
 proc rootPostHandler(request: nhttp.Request, response: nhttp.Response) =
   request.body = "root post success"
 
+proc getUsersHandler(request: nhttp.Request, response: nhttp.Response) =
+  request.body = "get users success"
+
+proc getUsersUsersHandler(request: nhttp.Request, response: nhttp.Response) =
+  request.body = "get users users success"
+
+proc getUsersCommentsFuncHandler(request: nhttp.Request, response: nhttp.Response) =
+  request.body = "get users comments func success"
+
 proc getUsersCommentsHandler(request: nhttp.Request, response: nhttp.Response) =
-  request.body = "get success"
+  request.body = "get users comments success"
 
 proc postUsersCommentsHandler(request: nhttp.Request, response: nhttp.Response) =
-  request.body = "post success"
+  request.body = "post users comments success"
 
 var testRouter = router.initRouter(notFound)
 let testResponse = nhttp.Response()
@@ -21,37 +30,58 @@ let testResponse = nhttp.Response()
 suite "router":
 
   test "adding and retrieving route":
-    testRouter.add("get", "/users/comments", getUsersCommentsHandler)
-    testRouter.add("post", "/users/comments", postUsersCommentsHandler)
-    let testRequestURI = uri.parseUri("http://example.com/users/comments")
-    var testRequest = nhttp.Request(uri: testRequestURI, m: "GET" )
-    testRouter.handle(testRequest, testResponse)
-    check(testRequest.body == "get success")
-    testRequest = nhttp.Request(uri: testRequestURI, m: "POST")
-    testRouter.handle(testRequest, testResponse)
-    check(testRequest.body == "post success")
+    echo "hi"
+    # testRouter.add("GET", "/users/comments", getUsersCommentsHandler)
+    # testRouter.add("POST", "/users/comments", postUsersCommentsHandler)
+    # testRouter.add("GET", "/users/comments/func", getUsersCommentsFuncHandler)
+    # var testRequestURI = uri.parseUri("http://example.com/users/comments")
+    # var testRequest = nhttp.Request(uri: testRequestURI, m: "GET" )
+    # testRouter.handle(testRequest, testResponse)
+    # check(testRequest.body == "get users comments success")
+    # testRequest = nhttp.Request(uri: testRequestURI, m: "POST")
+    # testRouter.handle(testRequest, testResponse)
+    # check(testRequest.body == "post users comments success")
+    # testRequestURI = uri.parseUri("http://example.com/users/comments/func")
+    # testRequest = nhttp.Request(uri: testRequestURI, m: "GET")
+    # testRouter.handle(testRequest, testResponse)
+    # check(testRequest.body == "get users comments func success")
+
+  test "adding and retrieving route with same words back to back":
+    testRouter.add("GET", "/users/users", getUsersUsersHandler)
+    testRouter.add("GET", "/users", getUsersHandler)
+    # var testRequestURI = uri.parseURI("http://example.com/users")
+    # var testRequest = nhttp.Request(uri: testRequestURI, m: "GET")
+    # testRouter.handle(testRequest, testResponse)
+    # check(testRequest.body == "get users success")
+
+  test "adding a path without a slash returns an error":
+    echo "hi"
+
 
   test "invoking not found for invalid routes":
-    var badTestRequestURI = uri.parseURI("http://example.com/posts/comment")
+    # var badTestRequestURI = uri.parseURI("http://example.com/posts/comment")
+    # var badTestRequest = nhttp.Request(uri: badTestRequestURI, m: "GET")
+    # testRouter.handle(badTestRequest, testResponse)
+    # check(badTestRequest.body == "not found")
+    # badTestRequestURI = uri.parseURI("http://example.com//")
+    # badTestRequest = nhttp.Request(uri: badTestRequestURI, m: "GET")
+    # testRouter.handle(badTestRequest, testResponse)
+    # check(badTestRequest.body == "not found")
+    var badTestRequestURI = uri.parseURI("http://example.com/")
     var badTestRequest = nhttp.Request(uri: badTestRequestURI, m: "GET")
     testRouter.handle(badTestRequest, testResponse)
-    check(badTestRequest.body == "not found")
-    badTestRequestURI = uri.parseURI("http://example.com//")
-    badTestRequest = nhttp.Request(uri: badTestRequestURI, m: "GET")
-    testRouter.handle(badTestRequest, testResponse)
-    check(badTestRequest.body == "not found")
-    badTestRequestURI = uri.parseURI("http://example.com/")
-    badTestRequest = nhttp.Request(uri: badTestRequestURI, m: "GET")
-    testRouter.handle(badTestRequest, testResponse)
+    echo "++++++++"
+    echo badTestRequest.body
     check(badTestRequest.body == "not found")
 
   test "adding and retrieving root route":
-    testrouter.add("get", "/", rootGetHandler)
-    testRouter.add("post", "/", rootPostHandler)
-    let rootRequestURI = uri.parseURI("http://example.com/")
-    var rootRequest = nhttp.Request(uri: rootRequestURI, m: "GET")
-    testRouter.handle(rootRequest, testResponse)
-    check(rootRequest.body == "root get success")
-    rootRequest = nhttp.Request(uri: rootRequestURI, m: "POST")
-    testRouter.handle(rootRequest, testResponse)
-    check(rootRequest.body == "root post success")
+    echo "hi"
+    # testrouter.add("get", "/", rootGetHandler)
+    # testRouter.add("post", "/", rootPostHandler)
+    # let rootRequestURI = uri.parseURI("http://example.com/")
+    # var rootRequest = nhttp.Request(uri: rootRequestURI, m: "GET")
+    # testRouter.handle(rootRequest, testResponse)
+    # check(rootRequest.body == "root get success")
+    # rootRequest = nhttp.Request(uri: rootRequestURI, m: "POST")
+    # testRouter.handle(rootRequest, testResponse)
+    # check(rootRequest.body == "root post success")
